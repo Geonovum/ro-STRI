bb587ca672d483464f3fee6e6bc48595ebc6f070
Monique van Scherpenzeel <m.vanscherpenzeel@geonovum.nl>
2023-11-08 17:03:02 +0100

    update config.js voor IMRO, IMROPT, SVBP en STRI

--- js/document-config.js
+++ js/document-config.js
@@ -1,16 +1,16 @@
 var documentConfig =
 {
-  title: "Standaard Toegankelijkheid Ruimtelijke Instrumenten (STRI2012)",
+  title: "Standaard Toegankelijkheid Ruimtelijke Instrumenten (STRI2012) versie 2,1,0",
   subtitle: "Bijlage 6 behorende bij de Regeling standaarden ruimtelijke ordening",
-  shortName: "STRI",
+  shortName: "stri",
   pubDomain: "ro",
   specStatus: "wv",
   specType: "no",
   license: "cc-by-nd",
   latestVersion: [
-    "docs.geostandaarden.nl/ro/STRI/"
+    "https://docs.geostandaarden.nl/ro/stri/"
   ],
-  publishDate: "2023-11-07",
+  publishDate: "2023-12-01",
   publishVersion: "2.1.0",
   thisVersion: [
     "https://geonovum.github.io/ro-STRI/"
@@ -18,16 +18,28 @@ var documentConfig =
   previousPublishDate: "2014-10-06",
   previousPublishVersion: "2.0",
   prevVersion: [
-    "docs.geostandaarden.nl/ro/def-no-STRI-20141006/"
+    "https://ro-standaarden.geonovum.nl/2012/STRI/2.0/STRI2012-v2.0.pdf"
   ],
   previousMaturity: "def",
   github: "https://github.com/geonovum/ro-STRI/",
-  issueBase: "https://github.com/geonovum/ro-STRI/issues/",
+  issueBase: "ro-standaarden@geonovum.nl",
   authors: [
     {
-      name: "Beheerder RO Standaarden",
+      name: "Arie Duindam",
       company: "Geonovum",
-      companyURL: ""
+      companyURL: "https://www.geonovum.nl/"
+    },
+    {
+      name: "Monique van Scherpenzeel",
+      company: "Geonovum",
+      companyURL: "https://www.geonovum.nl/"
+    }
+  ],
+  editors: [
+    {
+      name: "Monique van Scherpenzeel",
+      company: "Geonovum",
+      companyURL: "https://www.geonovum.nl/"
     }
   ]
 }
